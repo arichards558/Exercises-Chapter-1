74a83371e915d5818998817c907f1bdd9c39cf62
arichards558 <31348479+arichards558@users.noreply.github.com>
2017-08-29 09:26:19 -0500

    update

---
+++ MovieQouteInfo.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class MovieQouteInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"ITS TIME TO FREEEEEEEEZE!!! ~ Mr.Freeze, Batman and Robin (1997).");
+	}
+
+}
---
+++ MovieQoutes.java
@@ -0,0 +1,9 @@
+
+public class MovieQoutes {
+
+	public static void main(String[] args) {
+		System.out.println("ITS TIME TO FREEEEEEEEZE!!!");
+
+	}
+
+}
